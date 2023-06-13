246cd8160ea7299166c2850517ee057c5db6230e
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-06-13 15:35:32 -0300

    add architect working

--- admin/includes/controller/addArchitect.php
+++ admin/includes/controller/addArchitect.php
@@ -2,6 +2,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $url = 'https://' . rtrim($_SERVER['SERVER_NAME'], '/') . '/'."novosistemaarquitetos/";
+$pathUrl = "/var/www/vhosts/".$_SERVER['SERVER_NAME'] . '/httpdocs/novosistemaarquitetos/';
+
 ini_set('session.gc_maxlifetime', 3600); // 1 hora
 ini_set('session.cookie_lifetime', 3600);
 session_start();
@@ -16,45 +18,56 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
    
     if (isset($_FILES['photo'])) {
         $err = "";
-        // Diretório onde as imagens serão armazenadas
-        $target_dir = $url."files/architect-images/";
-        $target_file = $target_dir . strval($codigo) . "_" . basename($_FILES["photo"]["name"]);
+        $_FILES['photo']['name'] = $codigo. "-" . $_FILES['photo']['name'];
+
+        $target_dir = $pathUrl."files/architect-images/";
+        $target_file = $target_dir . basename($_FILES["photo"]["name"]);
+
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        print_r ($_FILES['photo']);
         $check = getimagesize($_FILES["photo"]["tmp_name"]);
         if($check !== false) {
             $uploadOk = 1;
         } else {
             $uploadOk = 0;
+            $err .= "tmp_name_null_";
         }
 
         // Check file size
         if ($_FILES["photo"]["size"] > 1000000) {
             $uploadOk = 0;
+            $err .= "size_high_";
+
         }
         if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
             $uploadOk = 0;
+            $err .= "incorrectly_filetype_";
+
         }
 
         // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
+            $err .= "upload_is_not_ok_";
             
         // if everything is ok, try to upload file
         } else {
             if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
                 echo "The file ". htmlspecialchars( basename( $_FILES["photo"]["name"])). " has been uploaded.";
+                $fotoUrl = $url . 'files/downloads/' . $_FILES['file']['name'];
             } else {
                 $err .= "err_on_image_";
+                $fotoUrl = $url."logo.png";
             }
         }   
+    } else{
+        $fotoUrl = $url."logo.png";
     }
     // Dados do formulário
     $arquiteto = $_POST['name'];
     $email = $_POST['email'];
     $senha = md5($_POST['password']);
     $dataCadastro = date('Y-m-d H:m:s'); 
-    $fotoUrl = isset($target_file) ? $target_file : $target_dir.'logo.png';
+    #$fotoUrl = isset($_FILES['file']['name'])? $url = $url . 'files/downloads/' . $_FILES['file']['name'] : $url."logo.png";
     $cpfCnpj = $_POST['cpf'];
     $rg = $_POST['rg'];
     $pis = $_POST['pis'];
@@ -75,15 +88,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Prepara a consulta SQL
 
     try {
-        $stmt = $mysqli->prepare("SELECT * FROM arquitetos WHERE email = ?");
-        $stmt->bind_param('sss', $email);
+        $stmt = $mysqli->prepare("SELECT * FROM arquitetos WHERE email = ? AND status = 'a'");
+        $stmt->bind_param('s', $email);
         $stmt->execute();
+        $stmt->store_result();
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
     
     if($stmt->num_rows > 0){
-        #header("location: ../../home.php?error=user_data_exists");
+        header("location: ../../home.php?error=user_data_exists");
     }
 
     else{
@@ -92,10 +106,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
         // Executa a consulta
         if ($stmt->execute()) {
+            $stmt->store_result();
+
             echo 'Arquiteto adicionado com sucesso!';
             header("location: ../../home.php?architect=sucess");
         } else {
-            #header("location: ../../home.php?architect=error");
+            header("location: ../../home.php?architect=error");
         }
     }
     
