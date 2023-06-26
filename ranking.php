ec9259f57698b7a8ff1fccf5381d809883d5487a
unknown <lucasdantassenac@gmail.com>
2023-06-26 15:09:20 -0300

    building ranking

--- ranking.php
+++ ranking.php
@@ -31,20 +31,11 @@ include_once 'includes/functions.php';
     $sqlVendedor         = "SELECT * FROM vendedores WHERE status = 'a' ORDER BY vendedor DESC;";
     $sqlArquiteto        = "SELECT * FROM arquitetos WHERE status = 'a' ORDER BY pontuacao DESC ;";
 
-    $sqlAllArchitects    = "SELECT * FROM arquitetos WHERE status = 'a' ORDER BY arquiteto;";
-    $sqlAllSellers       = "SELECT * FROM vendedores WHERE status = 'a' ORDER BY vendedor;";
-    $sqlAllOrders        = "SELECT pedidos.*, arquitetos.arquiteto FROM pedidos INNER JOIN arquitetos ON pedidos.idArquiteto = arquitetos.idArquiteto WHERE pedidos.status = 'a' ORDER BY dataCadastro DESC;";
-
-    $selectAllarchitects = mysqli_query($mysqli, $sqlAllArchitects);
-    $selectAllSellers    = mysqli_query($mysqli, $sqlAllSellers);
-    $selectAllOrders     = mysqli_query($mysqli, $sqlAllOrders);
-
     $selecionaPedido     = mysqli_query($mysqli, $sqlPedido); 
     $selecionaArquiteto  = mysqli_query($mysqli, $sqlArquiteto); 
     $selecionaVendedor   = mysqli_query($mysqli, $sqlVendedor);
 ?>
     <main>
-        
         <?php 
             if($_SESSION['userType'] == 'admin'):
                 include_once './admin/includes/popUps.php';
@@ -65,38 +56,37 @@ include_once 'includes/functions.php';
                 </div>
             </section>
         <?php endif ?>
-        <section class='last-requests contentSection '>
-            <div class='limiter'>
-                <h2 class='h4'>Pedidos</h2>
-                <table> 
-                    <thead>
-                        <tr class='thead'>
-                            <th>Pedido</th>
-                            <th>Cliente</th>
-                            <th>Data</th>
-                            <th>Valor</th>
-                            <th>Pontos</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($exibe = mysqli_fetch_array($selecionaPedido, MYSQLI_ASSOC)){  ?>
-                            <tr class='tcontent'>
-                                <td><?php echo $exibe['pedido']; ?></td>
-                                <td><?php custom_echo($exibe['cliente'], 25); ?></td>
-                                <td><?php echo formatTime("d/m/Y", $exibe['data']); ?></td> 
-                                <td><?php echo $exibe['valor']; ?></td> 
-                                <td><?php echo $exibe['pontos']; ?></td> 
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-                <a href='./consultarArquiteto.php' class='btn seeMore'>Ver todos</a>
-            </div>
-        </section>
         <section class='flexCenter pv'>
             <div class='limiter'>
                 <div class='row'>
-                    <div class='col c50'>
+                    <div class='col c33'>
+                        <h3>Ultimos pedidos adicionados</h3>
+                        <h2 class='h4'>Pedidos</h2>
+                        <table> 
+                            <thead>
+                                <tr class='thead'>
+                                    <th>Pedido</th>
+                                    <th>Cliente</th>
+                                    <th>Data</th>
+                                    <th>Valor</th>
+                                    <th>Pontos</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php while ($exibe = mysqli_fetch_array($selecionaPedido, MYSQLI_ASSOC)){  ?>
+                                    <tr class='tcontent'>
+                                        <td><?php echo $exibe['pedido']; ?></td>
+                                        <td><?php custom_echo($exibe['cliente'], 25); ?></td>
+                                        <td><?php echo formatTime("d/m/Y", $exibe['data']); ?></td> 
+                                        <td><?php echo $exibe['valor']; ?></td> 
+                                        <td><?php echo $exibe['pontos']; ?></td> 
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                        <a href='./consultarArquiteto.php' class='btn seeMore'>Ver todos</a>
+                    </div>
+                    <div class='col c33'>
                         <h3>Ultimos arquitetos adicionados</h3>
                         <table> 
                             <tbody>
@@ -109,7 +99,7 @@ include_once 'includes/functions.php';
                         </table>
                         <a href='./consultarArquiteto.php' class='btn seeMore'>Ver todos</a>
                     </div>
-                    <div class='col c50'>
+                    <div class='col c33'>
                         <h3>Ultimos vendedores adicionados</h3>
                         <table> 
                             <tbody>
@@ -128,3 +118,5 @@ include_once 'includes/functions.php';
 <?php
     require_once '../includes/footer.php';
 ?>
+
+
